611cf1de79fb5e63f1f4c2c848a0de9d82c973f3
gunjan1909 <bhanarkaranita@gmail.com>
2023-08-10 23:09:17 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,6 @@ export class AppComponent {
   serverElements: { type: string; name: string; content: string }[] = [];
   newServerName = '';
   newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
 }
 
 /*import { Component } from '@angular/core';
